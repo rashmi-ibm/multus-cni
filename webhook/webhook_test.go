dcae72fb56062e4324b73d5138b51a8d7b51420f
rashmi-ibm <54309456+rashmi-ibm@users.noreply.github.com>
2020-02-13 14:58:04 +0530

    Update webhook_test.go

--- webhook/webhook_test.go
+++ webhook/webhook_test.go
@@ -26,7 +26,7 @@ import (
 	"k8s.io/api/admission/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/intel/multus-cni/types"
+	nettypes "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 )
 
 var _ = Describe("Webhook", func() {
@@ -97,7 +97,7 @@ var _ = Describe("Webhook", func() {
 	})
 
 	DescribeTable("Network Attachment Definition validation",
-		func(in types.NetworkAttachmentDefinition, out bool, shouldFail bool) {
+		func(in nettypes.NetworkAttachmentDefinition, out bool, shouldFail bool) {
 			actualOut, err := validateNetworkAttachmentDefinition(in)
 			Expect(actualOut).To(Equal(out))
 			if shouldFail {
@@ -106,8 +106,8 @@ var _ = Describe("Webhook", func() {
 		},
 		Entry(
 			"empty config",
-			types.NetworkAttachmentDefinition{
-				Metadata: metav1.ObjectMeta{
+			nettypes.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: "some-valid-name",
 				},
 			},
@@ -115,8 +115,8 @@ var _ = Describe("Webhook", func() {
 		),
 		Entry(
 			"invalid name",
-			types.NetworkAttachmentDefinition{
-				Metadata: metav1.ObjectMeta{
+			nettypes.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: "some_invalid_name",
 				},
 			},
@@ -124,8 +124,8 @@ var _ = Describe("Webhook", func() {
 		),
 		Entry(
 			"invalid network config",
-			types.NetworkAttachmentDefinition{
-				Metadata: metav1.ObjectMeta{
+			nettypes.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: "some-valid-name",
 				},
 				Spec: types.NetworkAttachmentDefinitionSpec{
@@ -136,8 +136,8 @@ var _ = Describe("Webhook", func() {
 		),
 		Entry(
 			"valid network config",
-			types.NetworkAttachmentDefinition{
-				Metadata: metav1.ObjectMeta{
+			nettypes.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: "some-valid-name",
 				},
 				Spec: types.NetworkAttachmentDefinitionSpec{
@@ -151,8 +151,8 @@ var _ = Describe("Webhook", func() {
 		),
 		Entry(
 			"valid network config list",
-			types.NetworkAttachmentDefinition{
-				Metadata: metav1.ObjectMeta{
+			nettypes.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{
 					Name: "some-valid-name",
 				},
 				Spec: types.NetworkAttachmentDefinitionSpec{
